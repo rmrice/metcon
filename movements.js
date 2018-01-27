5b9fe0a666d1e2b2986078c8b9786688b8c30094
Rebecca Rice <rebecca.rice@mapbox.com>
2018-01-26 22:42:18 -0500

    initial commit; add movements & intial package

---
+++ movements.js
@@ -0,0 +1,13 @@
+var kettlebells = ['kettlebell swings', 'goblet squats', 'overhead squat with kettlebell', 'kettlebell snatch', 'turkish get-up'];
+var dumbbells = ['dumbbell snatch', 'dumbbell squat snatch', 'dumbbell thrusters', 'overhead lunge w/ dumbbell', 'man makers', 'dumbbell cleans'];
+var box = ['box jumps', 'box jump overs', 'burpee box jumps', 'burpee box jump overs', 'weighted box step ups'];
+var medball = ['wallballs'];
+var bodyweight = ['burpees', 'push ups', 'v-ups', 'sit ups', 'hand stand push ups'];
+var cardio = {
+  "rower": "cal row",
+  "bike": "cal bike",
+  "run": "meter run",
+  "ski-erg": "cal ski"
+};
+var pullupBar = ['pull ups', 'C2B pull ups', 'bar muscle ups', 'toes to bar', 'strict toes to bar', 'strict pull ups'];
+var rings = ['ring dips'];
